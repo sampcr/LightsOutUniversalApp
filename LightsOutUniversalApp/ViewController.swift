85ac05305d272a5caddea8cdf2410a747f5fe305
Christopher Robert Samp <sampcr@rose-hulman.edu>
2018-03-18 20:46:16 -0400

    Initial Commit

---
+++ LightsOutUniversalApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LightsOutUniversalApp
+//
+//  Created by Christopher Samp on 3/18/18.
+//  Copyright © 2018 Christopher Samp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
